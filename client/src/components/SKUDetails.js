0aff84f4e02f37617860159f7514de267deb6133
jessler91 <justinessler@fastmail.com>
2020-07-05 19:15:49 -0500

    save

---
+++ client/src/components/SKUDetails.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react'
+import { Container, Divider, Switch } from '@material-ui/core'
+
+class SKUDetails extends Component {
+
+    render() {
+        return (
+            <div className="text-gray">
+                <Container maxWidth="sm">
+                    <h1 style={{ textAlign: 'center' }}>SKUDetails</h1>
+        
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default SKUDetails
