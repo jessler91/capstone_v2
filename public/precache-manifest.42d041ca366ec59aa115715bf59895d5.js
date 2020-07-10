9debaa2d301de7e1dd41972c1172fcfbcccba6df
jessler91 <justinessler@fastmail.com>
2020-07-10 14:36:35 -0500

    saving

---
+++ public/precache-manifest.42d041ca366ec59aa115715bf59895d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f6c8c25979ad0b3a66dcc9d116c685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0147829a9f28deabf2f",
+    "url": "/static/css/main.bceaf175.chunk.css"
+  },
+  {
+    "revision": "0566b8ce3877db45f3a2",
+    "url": "/static/js/2.aa3b84fe.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.aa3b84fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0147829a9f28deabf2f",
+    "url": "/static/js/main.de10a402.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
