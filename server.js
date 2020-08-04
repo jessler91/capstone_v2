59968d6fcfd030975986cea0925b9a9af867f526
jessler91 <justinessler@fastmail.com>
2020-08-04 11:24:16 -0500

    save

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const path = require("path");
+const cors = require("cors");
+const path = require("path");
 const productsRoutes = require("./server/routes/products");
 const usersRoutes = require('./server/routes/users')
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,10 +13,10 @@ const port = process.env.PORT || 5000;
 // middleware - express, bodyParser, and routes middleware
 app.use(express.static("public"))
 app.use(bodyParser.json())
+app.use(cors());
 app.use(productsRoutes);
 app.use(usersRoutes)
 // app.use("/", productsRoutes);
-// app.use(express.static(path.join(_dirname, './public')))
 
 
 // Default Route
@@ -23,15 +25,15 @@ app.get('/', (req, res) => {
         `Welcome to Justins Capstone Server V2`)
 });
 
-// Serve Static Assits if in production
-// Handle production
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public/'));
-  
-    // Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-  }
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 
